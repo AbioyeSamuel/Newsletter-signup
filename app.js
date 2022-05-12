bd1089fb13bef68c510ea7f8342e2b21981f831d
AbioyeSamuel <101730798+AbioyeSamuel@users.noreply.github.com>
2022-05-12 02:41:34 -0700

    Add files via upload
    website was deployed and hosted on Heroku

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+  const url = "https://us11.api.mailchimp.com/3.0/lists/99d8cf7b19";
+
+  const options = {
+    method: "POST",
+    auth: "samuel:95eb3766b59b458301c29da24fe1141f-us11"
+  }
+
+  const request = https.request(url, options, function(response){
+      const status = response.statusCode;
+      if(status === 200){
+          res.sendFile(__dirname + "/success.html");
+      } else {
+          res.sendFile(__dirname + "/failure.html");
+      }
+
+      response.on("data", function(data){
+        console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.post("/failure", function(req, res){
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Server is running on port 3000");
+});
